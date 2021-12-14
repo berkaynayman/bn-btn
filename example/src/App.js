26d48a57cce35bff25026550c92a1308bf0c8747
berkaynayman <berkaynayman4@gmail.com>
2021-12-14 09:00:34 +0300

    v2.0.1

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react'
-
-import { ExampleComponent } from 'bn-btn'
+import { Button } from 'bn-btn'
 import 'bn-btn/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <Button type="primary" text="tıkla"/>
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import styles from './styles.module.css'
+import './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const Button = ({ type, text }) => {
+  return <button type={type}> {text} </button>
 }
